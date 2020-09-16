9ef12332da2c395a164bc0c4c79fecbfe5b3e159
abstractlyZach <zach3lee@gmail.com>
2020-09-16 00:32:20 -0700

    Add language option

--- src/learning_hypermodern/console.py
+++ src/learning_hypermodern/console.py
@@ -1,20 +1,24 @@
 import textwrap
 
 import click
-import requests
 
 from . import __version__
 from . import wikipedia
 
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
-
-
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hypermodern Python project."""
-    data = wikipedia.random_page()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
