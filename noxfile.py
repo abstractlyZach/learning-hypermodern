45ece73ed46bf1d0e6ddb69c1fb0861f2e016bad
abstractlyZach <zach3lee@gmail.com>
2020-09-17 15:09:59 -0700

    Add mypy

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import tempfile
 
 import nox
 
-nox.options.sessions = ["lint", "safety", "tests"]
+nox.options.sessions = ["lint", "safety", "mypy", "tests"]
 
 locations = ["src", "tests", "noxfile.py"]
 
@@ -18,6 +18,13 @@ def install_with_constraints(session, *args, **kwargs):
             external=True,
         )
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session(python=["3.8", "3.7"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
 
 
 @nox.session(python="3.8")
